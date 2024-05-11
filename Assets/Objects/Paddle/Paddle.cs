e9f2f02d57d9659d312da66868aca45a1295b24c
Jarryd Huntley <jarryd.huntley@gmail.com>
2024-05-11 14:44:09 -0400

    Finished stubbing out all the controls

--- Assets/Objects/Paddle/Paddle.cs
+++ Assets/Objects/Paddle/Paddle.cs
@@ -7,10 +7,14 @@ public class Paddle : MonoBehaviour
 
     public float speed = 3.0f;
     private Vector3 movement;
+    private bool inverted = false;
+
+    private PlayerInput playerInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInput = gameObject.GetComponent<PlayerInput>();
     }
     
     public void OnMove( InputAction.CallbackContext callbackContext)
@@ -18,17 +22,22 @@ public class Paddle : MonoBehaviour
         movement = callbackContext.ReadValue<Vector2>();
     }
 
+    public void OnInvert( InputAction.CallbackContext callbackContext)
+    {
+        inverted = callbackContext.action.IsPressed();
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + movement * speed * Time.deltaTime;
-        // if (Input.GetButton("Fire1"))
-        // {
-        //     transform.position = transform.position + new Vector3(0,1) *speed * Time.deltaTime;
-        // }
-        // if (Input.GetButton("Fire2"))
-        // {
-        //     transform.position = transform.position + new Vector3(0,-1) *speed * Time.deltaTime;
-        // }
+
+        if(playerInput.actions["Card1"].triggered)
+        {
+            Debug.Log("Card1 pressed");
+        }
+
     }
 }
