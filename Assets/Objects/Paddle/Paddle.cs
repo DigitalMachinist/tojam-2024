d4a802f7b8a8b948478dcc1ce2f7f52401d44a60
Jarryd Huntley <jarryd.huntley@gmail.com>
2024-05-10 22:45:43 -0400

    Added stub controls

--- Assets/Objects/Paddle/Paddle.cs
+++ Assets/Objects/Paddle/Paddle.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Paddle : MonoBehaviour
 {
+
+    public float speed = 3.0f;
+    private Vector3 movement;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    
+    public void OnMove( InputAction.CallbackContext callbackContext)
+    {
+        movement = callbackContext.ReadValue<Vector2>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Debug.Log("Fire1");
-        }
-        if (Input.GetButtonDown("Fire2"))
-        {
-            Debug.Log("Fire2");
-        }
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+        // if (Input.GetButton("Fire1"))
+        // {
+        //     transform.position = transform.position + new Vector3(0,1) *speed * Time.deltaTime;
+        // }
+        // if (Input.GetButton("Fire2"))
+        // {
+        //     transform.position = transform.position + new Vector3(0,-1) *speed * Time.deltaTime;
+        // }
     }
 }
